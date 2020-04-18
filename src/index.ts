c491bcfe3dc4a64a33824b5ce54639311d12875c
Cristi Mihai <cristi.mihai@gmail.com>
2020-04-18 17:25:16 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as Phaser from 'phaser';
 
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: 320,
+    height: 240,
+    zoom: 3,
     parent: 'canvas',
     physics: {
         default: 'arcade',
@@ -12,39 +13,95 @@ const config = {
             gravity: {y: 200}
         }
     },
+    render: {
+      pixelArt: true
+    },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 const game = new Phaser.Game(config);
 
-function preload ()
+function preload (this: Phaser.Scene)
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('bg1', require('./res/bg1.png'));
+    // this.load.spritesheet('p1', require('./res/piples1.png'), {
+    //     frameWidth: 16, frameHeight: 16
+    // });
+    this.load.image('tiles1', require('./res/piples1.png'))
+
 }
+
+
+let i: Phaser.GameObjects.Image;
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    this.add.image(160, 100, 'bg1');
+    // i = this.add.image(100, 150, 'p1');
 
-    const particles = this.add.particles('red');
+    const width = 20;
+    const height = 8;
 
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: {start: 1, end: 0},
-        blendMode: 'ADD'
+    let data = new Array(height);
+    for (let y = 0; y < height; y++) {
+        let row = data[y] = new Array(width);
+        for (let x = 0; x < width; x++) {
+            row[x] = Math.random()*5 | 0;
+        }
+    }
+
+    // this.cameras.main.setBounds(0, 0, 500, 100);
+    // this.cameras.main.setZoom(1);
+
+
+    const map = this.make.tilemap({
+        tileWidth: 16,
+        tileHeight: 16,
+        width,
+        height,
+        data
     });
 
-    const logo = this.physics.add.image(400, 100, 'logo');
+    const tileset = map.addTilesetImage('world1', 'tiles1');
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    const layer = map.createStaticLayer('layer', tileset, 0, 0);
+    layer.y = 80;
 
-    emitter.startFollow(logo);
+
+
+    // map.layer.data = data;
+    // map.data = data;
+
+
+    // const particles = this.add.particles('red');
+    //
+    // const emitter = particles.createEmitter({
+    //     speed: 100,
+    //     scale: {start: 1, end: 0},
+    //     blendMode: 'ADD'
+    // });
+
+    // const logo = this.physics.add.image(400, 100, 'logo');
+    // logo.setVelocity(100, 200);
+    // logo.setBounce(1, 1);
+    // logo.setCollideWorldBounds(true);
+
+    // emitter.startFollow(logo);
+
+}
+
+let f = 0;
+// let z = 1;
+function update() {
+
+    // z += 0.005;
+    // this.cameras.main.setZoom(z);
+
+    // f++;
+    // i.setFrame(f%4);
+    // i.frame.
 }
