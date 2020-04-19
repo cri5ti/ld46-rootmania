1857413183c132121099289efc2c2eb04ccb7ceb
Cristi Mihai <cristi.mihai@gmail.com>
2020-04-19 03:24:12 +0100

    atlas, input, drag, emitter

--- src/index.ts
+++ src/index.ts
@@ -28,11 +28,12 @@ const game = new Phaser.Game(config);
 function preload (this: Phaser.Scene)
 {
     this.load.image('bg1', require('./res/bg1.png'));
-    // this.load.spritesheet('p1', require('./res/piples1.png'), {
-    //     frameWidth: 16, frameHeight: 16
-    // });
     this.load.image('tiles1', require('./res/piples1.png'))
 
+    this.load.atlas('atlas', require('./res/atlas.png'), require('./res/atlas.json'));
+    // atlas.spritesheet('p1');
+    // atlas.spritesheet('p2');
+    // atlas.spritesheet('p3');
 }
 
 
@@ -41,56 +42,99 @@ let i: Phaser.GameObjects.Image;
 function create ()
 {
     this.add.image(160, 100, 'bg1');
-    // i = this.add.image(100, 150, 'p1');
 
     const width = 20;
     const height = 8;
 
-    let data = new Array(height);
-    for (let y = 0; y < height; y++) {
-        let row = data[y] = new Array(width);
-        for (let x = 0; x < width; x++) {
-            row[x] = Math.random()*5 | 0;
+    const map = this.make.tilemap({
+        key: 'map',
+        tileWidth: 16,
+        tileHeight: 16,
+        data: new Array(height).fill(0).map(() => new Array(width).fill(0))
+    });
+    const tileset = map.addTilesetImage('set1', 'tiles1', 16, 16);
+
+    const layer = map.createDynamicLayer('layer', 'set1', 0, 80);
+
+    layer.layer.width = width;
+    layer.layer.height = height;
+
+    for (let y = 0; y < layer.layer.height; y++) {
+        for (let x = 0; x < layer.layer.width; x++) {
+            let t = layer.getTileAt(x, y, true);
+
+            const ix = Math.random()*5 | 0;
+            t.index = ix;
+            t.flipX = Math.random()*2 > 1 ? true : false;
+            t.flipY = Math.random()*2 > 1 ? true : false;
+            layer.putTileAt(t, x, y);
         }
     }
 
-    // this.cameras.main.setBounds(0, 0, 500, 100);
-    // this.cameras.main.setZoom(1);
 
 
-    const map = this.make.tilemap({
-        tileWidth: 16,
-        tileHeight: 16,
-        width,
-        height,
-        data
+
+
+
+
+
+    const particles = this.add.particles('atlas');
+
+    const emitter = particles.createEmitter({
+        frame: {
+          frames: [
+              'particle1',
+              'particle2',
+              'particle3'
+          ]
+        },
+        // speed: 200,
+        // maxParticles: 30,
+        // lifespan: 0,
+        // delay: 100,
+
+
+        // blendMode: 'ADD'
+        speed: 1,
+        // delay: 50,
+        frequency: 50,
+        scale: {start: 1, end: 2},
+        alpha: {start: 1, end: 0},
+        lifespan: 1000,
+        gravityY: -50,
+        gravityX: -10,
+        // speedX: -20000,
+        blendMode: 'ADD',
+        on: false,
+        // visible: false,
     });
-
-    const tileset = map.addTilesetImage('world1', 'tiles1');
-
-    const layer = map.createStaticLayer('layer', tileset, 0, 0);
-    layer.y = 80;
-
-
-
-    // map.layer.data = data;
-    // map.data = data;
-
-
-    // const particles = this.add.particles('red');
-    //
-    // const emitter = particles.createEmitter({
-    //     speed: 100,
-    //     scale: {start: 1, end: 0},
-    //     blendMode: 'ADD'
-    // });
 
     // const logo = this.physics.add.image(400, 100, 'logo');
     // logo.setVelocity(100, 200);
     // logo.setBounce(1, 1);
     // logo.setCollideWorldBounds(true);
 
-    // emitter.startFollow(logo);
+
+    const image = this.add.image(150, 150, 'atlas', 'p1');
+    image.setInteractive();
+    this.input.setDraggable(image);
+
+
+    emitter.startFollow(image);
+
+
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+        emitter.on = true;
+    });
+
+    this.input.on('dragend', function (pointer, gameObject, dragX, dragY) {
+        // emitter.active = false;
+        emitter.on = false;
+    });
+
+
 
 }
 
@@ -105,3 +149,19 @@ function update() {
     // i.setFrame(f%4);
     // i.frame.
 }
+
+
+type int = number;
+
+class Map
+{
+    w:int;
+    h:int;
+
+
+}
+
+
+
+
+
